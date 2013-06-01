83c873bc69b48c803cc80515b3f0af693658e6d3
eklotek <eklotek@gmail.com>
2013-06-01 12:02:38 -0500

    Reorganization

--- Pendulum.java
+++ Pendulum.java
@@ -1,37 +1,69 @@
 /** Pendulum.java
  *  This program simulates a double pendulum using a Runge-Kutta approximation.
+ *
+ *  This file serves two purposes:
+ *  1. Contains the main method and initializes the simulation
+ *  2. Serves as a Pendulum object
  */
 
-public class Pendulum
+//  Laziness to avoid writing "Math."
+import static java.lang.Math.sin;
+import static java.lang.Math.cos;
+
+public class Pendulum implements Runnable
 {
     public static void main(String[] args)
     {
-        Pendulum p = new Pendulum(1, 0.9, 1, 0.9, 179.999, 179.999, 0, 0);
+        Pendulum p = new Pendulum(1, 0.9, 1, 0.9, 179.999, 179.999, 0, 0, 0.001);
         for (int i = 0; i < 10; i++)
         {
-            p.step(0.001);
-            System.out.printf("(%f, %f)%n", p.getX(), p.getY());
+            p.step();
+            System.out.println(p.get());
         }
     }
+
+    public void run()
+    {
+    }
+
+    private static final double g = 9.80665;
+    private double L1, L2, m1, m2, t1, t2, w1, w2, h;
 
     public Pendulum(
         double length1, double length2,
         double mass1, double mass2,
         double theta1, double theta2,
-        double omega1, double omega2)
+        double omega1, double omega2,
+        double stepsize)
     {
+        L1 = length1;
+        L2 = length2;
+        m1 = mass1;
+        m2 = mass2;
+        t1 = theta1;
+        t2 = theta2;
+        w1 = omega1;
+        w2 = omega2;
+        h = stepsize;
     }
 
-    public void step(double delta)
+    public Pair get()
     {
+        return new Pair(0, 0);
     }
 
-    public double getX()
+    public void step()
     {
-        return 0;
+        
     }
 
-    public double getY()
+    private double f1(double t1, double t2, double w1, double w2)
+    {
+        return (g*(m2*sin(t2)*cos(t1-t2)-(m1+m2)*sin(t1))-m2*(L2*w1*w1*
+            cos(t1-t2))*sin(t1-t2))/(L1*(m1+m2*sin(t1-t2)*sin(t1-t2)));
+    }
+
+    private double f2(double t1, double t2, double w1, double w2)
     {
         return 0;
     }
---
+++ PendulumProducer.java
@@ -0,0 +1,51 @@
+/** PendulumProducer.java
+ *  Works as a Producer adding to a bounded blocking queue stored internally.
+ *  The consumer simply calls the get() method here to grab the next position.
+ */
+
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+
+public class PendulumProducer implements Runnable
+{
+    private Pendulum pendulum;
+    private BlockingQueue<Pair> queue;
+
+    public PendulumProducer(Pendulum p, int capacity)
+    {
+        pendulum = p;
+        queue = new LinkedBlockingQueue<Pair>(capacity);
+    }
+
+    public Pair get()
+    {
+        Pair result = null;
+        try
+        {
+            result = queue.take();
+        }
+        catch (InterruptedException e)
+        {
+            e.printStackTrace();
+            System.exit(1);
+        }
+        return result;
+    }
+
+    public void run()
+    {
+        try
+        {
+            while (true)
+            {
+                pendulum.step();
+                queue.put(pendulum.get());
+            }
+        }
+        catch (InterruptedException e)
+        {
+            e.printStackTrace();
+            System.exit(1);
+        }
+    }
+}
