8dccdea2e10f996d4369a15020ba75c504d8c881
eklotek <eklotek@gmail.com>
2013-05-31 21:02:14 -0500

    Comment + code

---
+++ Pendulum.java
@@ -0,0 +1,11 @@
+/** Pendulum.java
+ *  This program simulates a double pendulum using a Runge-Kutta approximation.
+ */
+
+public class Pendulum
+{
+    public static void main(String[] args)
+    {
+        //  TODO
+    }
+}
